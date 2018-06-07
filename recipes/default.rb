c752e37d1c33b7c64f785680fb60a806ff9869ed
Your Name <you@example.com>
2018-06-07 09:46:17 +0000

    tomcat 1st commit

---
+++ recipes/default.rb
@@ -0,0 +1,38 @@
+#
+# Cookbook:: tomcat
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
+package "java"
+package "java-devel"
+execute 'tomcat' do
+ command '/usr/bin/wget http://www-us.apache.org/dist/tomcat/tomcat-7/v7.0.88/bin/apache-tomcat-7.0.88.tar.gz'
+ action :run
+end
+execute 'tomcat_chown' do
+command 'chown -R satishk:satishk apache-tomcat-7.0.88.tar.gz'
+action :run
+end
+execute 'tomcat_install' do
+command '/usr/bin/tar xvfz apache-tomcat-7.0.88.tar.gz -C /home/satishk'
+ action :run
+end
+execute 'rmove_tomcat' do
+command 'rm -rf /home/satishk/apache-tomcat-7.0.88/conf/tomcat-users.xml'
+action :run
+end
+file '/home/satishk/apache-tomcat-7.0.88/conf/tomcat-users.xml' do
+content '<tomcat-users>
+<role rolename="manager-gui"/>
+<user username="tomcat" password="tomcat" roles="manager-gui"/>
+</tomcat-users>'
+end
+execute 'tomcat_chown' do
+command 'chown -R satishk:satishk /home/satishk/apache-tomcat-7.0.88'
+action :run
+end
+execute 'tomcat_run' do
+command '/home/satishk/apache-tomcat-7.0.88/bin/startup.sh'
+action :run
+end
